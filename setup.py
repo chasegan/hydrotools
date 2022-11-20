10e7b6144c215e12d8acde66827f418e869b3564
eganc <thecosmologist@gmail.com>
2022-11-20 18:13:17 +1000

    Version 0.0.2

--- setup.py
+++ setup.py
@@ -3,9 +3,11 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+exec(open('hydrotools/version.py').read())
+
 setuptools.setup(
     name="hydrotools",
-    version="0.0.1",
+    version=__version__,
     author="Chas Egan",
     author_email="thecosmologist@gmail.com",
     description="A code torrent of washing sprawl which in watery worlds is able of all.",
